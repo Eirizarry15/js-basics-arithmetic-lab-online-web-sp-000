5f735ca9580cc9df2a0440411d9a427d199f7241
Edgar Irizarry <eirizarry15@yahoo.com>
2019-04-03 17:50:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,40 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+function add(a,b){
+  return a + b;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function inc(n){
+  return n += 1;
+}
+
+function dec(n){
+  return n -= 1;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
+
+
+
+
+
